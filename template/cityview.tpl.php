aa062011e7c89e18eac6f1a50e44aa857cf4b04f
Andreas Kasch <andreas.kasch@googlemail.com>
2011-11-17 14:54:23 +0100

    anfang der city anzeige

--- template/cityview.tpl.php
+++ template/cityview.tpl.php
@@ -1,4 +1,12 @@
 			<div id="landscape">
-				
+				<?php
+					$self = fs_getSelfCheckinOne();
+					//printarray($self);
+					$venueID = $self->venue->id;
+					printarray(db_citizenInVenue($venueID));
+					printarray(db_playersInVenue($venueID));
+					printCityValues(db_citizenInVenue($venueID),1,$venueID);
+					//echo db_citizenInVenue($venueID);
+				?>
 				<?php //require_once("canvas-anim.html"); ?>
 			</div>

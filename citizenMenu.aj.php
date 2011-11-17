9db83403da536ace6c20ea9b6b787eacf00f7567
PeterTheOne <petertheone@gmail.com>
2011-11-17 14:52:07 +0100

    add- and removeCitizen works with different jobs! show citizens table also when there are no citizens createtable.php now using InnoDB which supports transactions fixed db_hasErrors improved db_citizenGroupJob($userId, $venueId) fixed citizenMenu.js bug (multiple loading of files)

--- citizenMenu.aj.php
+++ citizenMenu.aj.php
@@ -16,32 +16,38 @@ $venue = $latestCheckin->venue;
 //TODO: error handling
 db_selectUser($_SESSION['userid']);
 
+
 echo "<script type=\"text/javascript\" src=\"script/citizenMenu.js\"></script>";
 echo "<div style=\"padding-top:50px\">";
+
 //TODO: remove '* 1000' it is only for debug
 if (!fs_isCheckedIn($latestCheckin->createdAt)) {
 	echo "<p>Du kannst keine Bürger plazieren oder entfernen, dein checkin ist zu lange her.</p>";
 } else {
 	//db_selectCitizenOfVenue($_SESSION['userid'], $venue->id);
-	db_citizenGroupJob($_SESSION['userid'], $venue->id);
+	$citizenGroupJob = db_citizenGroupJob($_SESSION['userid'], $venue->id);
 	//TODO: auch jobs auflisten in denen keine bürger sind.
 	//TODO: machen das der addCitizen button auch wirklich diesen job hinzufügt
-	if (count($citizenGroupJob) < 1) {
-		echo "<p>Es gibt keine Bürger in dieser Stadt</p>";
-	} else {
-		echo "<table border=\"1\">";
-		echo "<tr><td>job</td><td>count</td><td>-</td><td>+</td></tr>";
-		foreach ($citizenGroupJob as $count => $job) {
-			echo "<tr>";
-			echo "<td>" . $job . "</td>";
-			echo "<td>" . $count . "</td>";
-			echo "<td><span id=\"removeCitizen\">Bürger aus Stadt nehmen?</span></td>";
-			echo "<td><span id=\"addCitizen\">Bürger plazieren?</span></td>";
-			echo "</tr>";
+	echo "<table border=\"1\">";
+	echo "<tr><td>job</td><td>desc</td><td>count</td><td>-</td><td>+</td></tr>";
+	foreach ($citizenGroupJob as $job) {
+		echo "<tr>";
+		echo "<td>" . $job['Name'] . "</td>";
+		echo "<td>" . $job['Description'] . "</td>";
+		echo "<td>" . $job['jobCount'] . "</td>";
+		if ($job['jobCount'] > 0) {
+			echo "<td><span class=\"id\" style=\"display: none\">". $job['ID'] ."</span><span class=\"removeCitizen\">Bürger aus Stadt nehmen?</span></td>";
+		} else {
+			echo "<td>-</td>";
 		}
-		echo "</table>";
+		if ($user->unusedCitizen > 0) {
+			echo "<td><span class=\"id\" style=\"display: none\">". $job['ID'] ."</span><span class=\"addCitizen\">Bürger plazieren?</span></td>";
+		} else {
+			echo "<td>-</td>";
+		}
+		echo "</tr>";
 	}
+	echo "</table>";
 }
 echo "</div>";
-
 ?>
--- script/citizenMenu.js
+++ script/citizenMenu.js
@@ -1,17 +1,19 @@
 $(function(){
-	$("#addCitizen").bind("touchend", function(){
-		$("#addCitizen").click();
+	$(".addCitizen").css("cursor","pointer");
+	$(".removeCitizen").css("cursor","pointer");
+	$(".addCitizen").bind("touchend", function(){
+		$(".addCitizen").click();
 	});
-	$("#addCitizen").click(function(){
-		$.post("addCitizen.aj.php", function(data){
+	$(".addCitizen").click(function(){
+		$.post("addCitizen.aj.php", {id: $(this).parent().children(".id").html()}, function(data){
 			$("#landscape").html(data);
 		});
 	});
-	$("#removeCitizen").bind("touchend", function(){
-		$("#removeCitizen").click();
+	$(".removeCitizen").bind("touchend", function(){
+		$(".removeCitizen").click();
 	});
-	$("#removeCitizen").click(function(){
-		$.post("removeCitizen.aj.php", function(data){
+	$(".removeCitizen").click(function(){
+		$.post("removeCitizen.aj.php", {id: $(this).parent().children(".id").html()}, function(data){
 			$("#landscape").html(data);
 		});
 	});

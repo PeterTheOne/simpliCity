5db7b2c1dd2a2948de2f819cd5d52ff3cea1be3d
Andreas Kasch <andreas.kasch@googlemail.com>
2012-01-22 17:45:44 +0100

    neue icons

--- template/element_menu.tpl.php
+++ template/element_menu.tpl.php
@@ -1,8 +1,8 @@
 				<div id="upperLeft">
-					<a class="button" id="cityBtn" href="?view=city"><img src="images/interface/randomButton.png" alt="unknown"/></a>
-					<a class="button" id="guildBtn" href="?view=list"><img src="images/interface/randomButton.png" alt="unknown"/></a>
-					<a class="button" id="statsBtn" href="?view=stats"><img src="images/interface/randomButton.png" alt="unknown"/></a>
+					<a class="button" href="?view=city"><img src="images/interface/city.png" alt="unknown"/></a>
+					<a class="button" href="?view=list"><img src="images/interface/cityList.png" alt="unknown"/></a>
+					<a class="button" href="?view=stats"><img src="images/interface/playerStats.png" alt="unknown"/></a>
 				</div>
 				<div id="lowerRight">
-					<a class="button" id="setupBtn" href="?view=setup"><img src="images/interface/randomButton.png" alt="unknown"/></a>
+					<a class="button" href="?view=help"><img src="images/interface/help.png" alt="unknown"/></a>
 				</div>

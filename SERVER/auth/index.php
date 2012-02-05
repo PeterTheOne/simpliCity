e9b6b12c3f70b792695733c77bb66153a489ebba
Peter Grassberger <petertheone@gmail.com>
2012-02-05 17:47:51 +0100

    cleanup in SERVER/auth/index.php

--- SERVER/auth/index.php
+++ SERVER/auth/index.php
@@ -1,32 +1,41 @@
-<?php
+<?
+// config
+
+// see: https://developer.foursquare.com/docs/oauth.html
+// for every uri a new CLIENT_ID must be created.
+define("CLIENT_ID", "GPP0FG0DZI3ES5JKLNBJWZDMEITYI2XECBVTSW2GL3ZJCHGT");
+define("CLIENT_SECRET", "5P3ABYS1CSVHXVEEAHWZP1A42DWZ4DTXROA5SAFCTXZCJNMX");
+define("REDIRECT_URI", "http://petergrassberger.at/pro5/prototypes/foursquare-api_php-foursquare/");
+
+// includes and functions
+
+// see: https://github.com/stephenyoung/php-foursquare
+require_once("FoursquareAPI.class.php");
+
+function sanitize($str) {
+	return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
+
+// init
+
 session_start();
 
 if (isset($_GET['host'])) {
 	$_SESSION['host'] = sanitize($_GET['host']);
 }
 
-//https://github.com/stephenyoung/php-foursquare
-require_once("FoursquareAPI.class.php");
-
-// docu: https://developer.foursquare.com/docs/oauth.html
-// für jede neue uri muss eine neue Client-ID angelegt werden.
-
-define("CLIENT_ID",     "GPP0FG0DZI3ES5JKLNBJWZDMEITYI2XECBVTSW2GL3ZJCHGT");
-define("CLIENT_SECRET",     "5P3ABYS1CSVHXVEEAHWZP1A42DWZ4DTXROA5SAFCTXZCJNMX");
-define("REDIRECT_URI",     "http://petergrassberger.at/pro5/prototypes/foursquare-api_php-foursquare/");
-
-function sanitize($str) {
-	return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
-}
-
 $foursquare = new FoursquareAPI(CLIENT_ID, CLIENT_SECRET);
 
-if(array_key_exists("code", $_GET)){
+if(isset($_GET['code'])) {
 	$token = $foursquare->GetToken(sanitize($_GET['code']), REDIRECT_URI);
+} else if (isset($_POST['code'])) {
+	$token = $foursquare->GetToken(sanitize($_POST['code']), REDIRECT_URI);
 }
 
+// display
+
 	if (!isset($token)) { 
-		?>
+?>
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -56,26 +65,26 @@ if(array_key_exists("code", $_GET)){
 	</head>
 	<body>
 		<div id="container">
-		<img src="simplicity.png" alt="SimpliCity"/>
-		<script type="text/javascript">
-			$(function(){
-				$("#jsactivate").hide();
-				$("#authlink").show();
-			});
-		</script>
-		<p id="jsactivate">
-			Sie m&uuml;ssen Javascript aktivieren um das Spiel zu spielen!
-		</p>
-		<?php
-		echo "<p style=\"display:none\" id=\"authlink\"><a href='" . 
-			$foursquare->AuthenticationLink(REDIRECT_URI) . 
-			"'><img src=\"foursquare.png\" alt=\"Connect to this app via Foursquare\" /></a></p>";
-			
-		?>
+			<img src="simplicity.png" alt="SimpliCity"/>
+			<script type="text/javascript">
+				$(function(){
+					$("#jsactivate").hide();
+					$("#authlink").show();
+				});
+			</script>
+			<p id="jsactivate">
+				Sie m&uuml;ssen Javascript aktivieren um das Spiel zu spielen!
+			</p>
+<?php
+			echo "<p style=\"display:none\" id=\"authlink\"><a href='" . 
+				$foursquare->AuthenticationLink(REDIRECT_URI) . 
+				"'><img src=\"foursquare.png\" alt=\"Connect to this app via Foursquare\" /></a></p>";
+				
+?>
 		</div>
 	</body>
 </html>
-		<?php
+<?php
 	} else {
 		//echo "<p id=\"token\">$token</p>";
 		header('Location: http://' . $_SESSION['host'] . "?token=$token");
